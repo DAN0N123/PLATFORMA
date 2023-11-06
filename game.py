22c6ea6dbb8ce8bc65f239e1880856f3a0683f3e
DAN0N123 <filipskubis01@wp.pl>
2023-11-06 15:09:13 +0100

    added platforms and basic physics

--- game.py
+++ game.py
@@ -39,18 +39,31 @@ class game_object():
         pygame.draw.rect(screen, self.color, self.object)
 
     
+class platform_object(game_object):                 
+    def __init__(self, x, y, width, height, color, image=None):
+        super().__init__(x, y, width, height, color, image)
+    
+
+
+    def draw(self, screen):
+        super().draw(screen)
+
 
 class Player():
     def __init__(self):
-        self.height = 80
-        self.width = 30
+        self.onplatform = False
+        self.height = 87
+        self.width = 100
         self.speed = 3
         self.model = testing
         self.x = 768
         self.y = 664
+        self.fallingdown = False
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
         self.jumping = False
-        self.jump_height = 20
+        self.jump_height = 30
         self.gravity = 1
+        self.tempy = 0
 
     def draw(self, screen):
         screen.blit(self.model, (self.x, self.y))
@@ -67,38 +80,36 @@ class Player():
                 self.x += self.speed
         
         if keys[pygame.K_SPACE] and not self.jumping:
+            self.tempy = self.y
             self.jumping = True
-            self.jump_velocity = -self.jump_height  # Start the jump with an upward velocity
+            self.jump_velocity = -self.jump_height
 
         if self.jumping:
+            if self.y <= self.tempy - 465:
+                self.fallingdown = True
             self.y += self.jump_velocity
             self.jump_velocity += self.gravity
-
-            if self.y >= 664:  
+            
+            if self.y >= 664:
+                self.fallingdown = False
                 self.y = 664
                 self.jumping = False
                 self.jump_velocity = 0
 
-        if self.y > 664:
-            self.y = 664
-            self.jumping = False
+    def collision_with_platform(self, platform: platform_object):
+        if self.hitbox.colliderect(platform.object):
+            return True
                 
-                
-                    
+platforms = []     
 
-
-
-# def anti_gravity(object, speed):
-#     if object.y > 564:
-#         object.y -= speed
-#     else:
-#         object.y = 564
     
 
 my_player = Player()    
 floor = game_object(0, 751, 1536, 200, yellow)
-
+platforms.append(platform_object(1000, 440, 200, 20, black))
+platforms.append(platform_object(200, 600, 300, 20, black))
 last_action_time = 0
+check = []
 
 
 while running:
@@ -108,12 +119,27 @@ while running:
     screen.fill((89, 201, 250)) 
 
     current_time = pygame.time.get_ticks()
-
+    
     floor.draw(screen)
     my_player.draw(screen)
     my_player.movement()
+    my_player.hitbox.x = my_player.x
+    my_player.hitbox.y = my_player.y 
 
-    # if current_time - last_action_time >= 1000:
+
+    for a_platform in platforms:
+        a_platform.draw(screen)
+        if my_player.collision_with_platform(a_platform) and my_player.fallingdown:
+            my_player.y = a_platform.y - my_player.height
+            my_player.jumping = False
+            my_player.jump_velocity = 0
+        elif my_player.y < 664 and not my_player.collision_with_platform(a_platform):
+            my_player.jump_height = 0
+            my_player.jumping = True
+        if my_player.y >= 664:
+            my_player.jump_height = 30
+            my_player.y = 664
+            my_player.jumping = False
     
 
     clock.tick(144)
